4e251030133cb1e0fd99fd320112047aa5b8ae29
Cortes945 <25030313@itesa.edu.mx>
2026-09-17 14:49:33 -0600

    Verión_02:Metodo llenar

---
+++ Arreglos.Logica/MiArreglo.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arreglos.Logica
+{
+    public class MiArreglo
+    {
+        //Atributos o campos
+        private int _tope;
+        private int[] _arreglo;
+
+        //Constructor
+        public MiArreglo(int n)
+        {
+            N = n;
+            _arreglo = new int[N];
+            _tope = 0;
+        }
+
+        //Propiedades
+        public int N { get; }
+        public bool EstaLleno => _tope == N;
+
+        public bool EstaVacio => _tope == 0;
+
+        //Metodos
+        //Metodos Llenar
+        public void Llenar(int minimo, int maximo)
+        {
+            Random oRandom = new Random();
+            for (int i = 0; i < N; i++)
+            {
+                _arreglo[i] = oRandom.Next(minimo, maximo);
+            }
+            _tope = N;
+        }
+
+
+        //Metodo ToString
+
+        public override string ToString()
+        {
+            if (EstaVacio)
+            {
+                return "Esta vacio";
+            }
+
+            string cadena=string.Empty;
+            int contador = 0;
+            for (int i = 0;i < _tope-1; i++)
+            { 
+              //Cadena = cadena + _arreglo[i];
+              cadena += $"{_arreglo[i]}\t";
+                contador++;
+                if (contador > 9)
+                {
+                    contador = 0;
+                    cadena += "\n";
+                }
+                
+
+            }
+
+            return cadena;  
+        }
+
+    }
+}
